1576ce37a128247b74ee71be9c6b9f4ad07ada80
Ahmad Sawi <AhmadSawi@pc-203-32.customer.firhill.crm.lan>
2019-02-06 18:14:52 +0000

    Chapter 7

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+def add_category(request):
+	form = CategoryForm()
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		if form.is_valid():
+			category = form.save(commit=True)
+			print(category, category.slug)
+			return index(request)
+		else:
+			print(form.errors)
+	return render(request, 'rango/add_category.html', {'form': form})
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+	form = PageForm()
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if category:
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				return show_category(request, category_name_slug)
+		else:
+			print(form.errors)
+	context_dict = {'form':form, 'category': category}
+	return render(request, 'rango/add_page.html', context_dict)
+
 def show_category(request, category_name_slug):
 	context_dict = {}
 	try:
@@ -22,7 +54,7 @@ def index(request):
 def about(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage is the same as {{ boldmessage }} in the template!
-	context_dict = {'Ahmad': "This tutorial has been put together by Ahmad Sawi"}
+	context_dict = {'Anas': "This tutorial has been put together by Anas Yahya"}
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier.
 	# Note that the first parameter is the template we wish to use.
