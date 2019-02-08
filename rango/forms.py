e71b4282ddf5115f61af63e2278f6a2a0b865de9
Ahmad Sawi <AhmadSawi@pc-203-32.customer.firhill.crm.lan>
2019-02-08 01:06:18 +0000

    Chapter 9

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category , UserProfile
+from django.contrib.auth.models import User
+
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
     help_text = "Please enter the category name.")
@@ -9,6 +11,7 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ('name',)
+
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
                          help_text = "Please enter the title of the page.")
@@ -26,3 +29,14 @@ class PageForm(forms.ModelForm):
             url = 'http://' + url
             cleaned_data['url'] = url
             return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
